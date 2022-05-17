28c1b23d4aaa590dbc407b2b9aad83adad260714
Marc AlKareh <ma7748@student.st-chris.net>
2022-05-17 16:18:30 +0300

    fetched data from API and got the questions

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-import TOKEN from "./config.js";
-
 const score = document.querySelector(".score-number");
 const question = document.querySelector(".question-text");
 const answerOptions = document.querySelectorAll(".option");
 
 const getQuestions = async function () {
-  const res = await fetch(
-    `https://opentdb.com/api.php?amount=10&token=${TOKEN}`
-  );
+  const res = await fetch(`https://opentdb.com/api.php?amount=10`);
 
   const data = await res.json();
   console.log(data);
