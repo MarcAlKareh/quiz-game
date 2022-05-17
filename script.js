c0f81cceb4b4995c1cb41b17af8d7ff8b01c2e0b
Marc AlKareh <ma7748@student.st-chris.net>
2022-05-17 16:49:07 +0300

    added relevant information to object for later use

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const score = document.querySelector(".score-number");
-const question = document.querySelector(".question-text");
+const questionEl = document.querySelector(".question-text");
 const answerOptions = document.querySelectorAll(".option");
+
+const questionData = {
+  questions: [],
+  answers: [],
+};
 
 const getQuestions = async function () {
   const res = await fetch(`https://opentdb.com/api.php?amount=10`);
 
-  const data = await res.json();
+  const { results: data } = await res.json();
   console.log(data);
+
+  data.forEach(function (obj) {
+    questionData.questions.push(obj.question);
+    questionData.answers.push([...obj.incorrect_answers, obj.correct_answer]);
+  });
 };
+
 getQuestions();
+console.log(questionData);
