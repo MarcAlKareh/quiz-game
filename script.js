7f95c0298381327396b00b11d5249501700c7175
Marc AlKareh <ma7748@student.st-chris.net>
2022-05-18 16:20:05 +0300

    got question and answers to appear on screen, howevere there is a bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const score = document.querySelector(".score-number");
-const questionEl = document.querySelector(".question-text");
-const answerOptions = document.querySelectorAll(".option");
+const score = document.querySelector('.score-number');
+const questionEl = document.querySelector('.question-text');
+const optionsContainer = document.querySelector('.answer-options');
 
 const questionData = {
   questions: [],
@@ -13,11 +13,30 @@ const getQuestions = async function () {
   const { results: data } = await res.json();
   console.log(data);
 
-  data.forEach(function (obj) {
+  data.forEach(obj => {
     questionData.questions.push(obj.question);
-    questionData.answers.push([...obj.incorrect_answers, obj.correct_answer]);
+    questionData.answers.push({
+      correctAnswer: obj.correct_answer,
+      options: [...obj.incorrect_answers, obj.correct_answer],
+    });
   });
+  console.log(questionData.questions);
 };
 
-getQuestions();
-console.log(questionData);
+const setQuestionAndAnswer = async function () {
+  await getQuestions();
+  console.log(questionData.answers);
+  questionEl.textContent = questionData.questions[0];
+
+  let html = ``;
+
+  questionData.answers.forEach(optionsData => {
+    optionsData.options.forEach(option => {
+      html += `<button class="option">${option}</button>`;
+    });
+  });
+
+  optionsContainer.insertAdjacentHTML('beforeend', html);
+};
+
+setQuestionAndAnswer();
